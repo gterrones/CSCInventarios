90c94862131c20d43fdc1073d3ea3f1cbbc92ab3
gterrones <gilmerterrones2000@gmail.com>
2015-06-26 22:38:22 -0500

    Actualizacion declases
    dominio

---
+++ SistemaWeb/CSCInventarios/Dominio/EEstacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dominio
+{
+    public class EEstacion
+    {
+        private int estacion_id;
+        private String estacion_nombre;
+        private String estacion_zona;
+        private String estacion_cordenadas;
+        private String estacion_referencia;
+    }
+}
